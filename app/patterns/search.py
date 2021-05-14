9ce884a4690033d4cb45f41468f7436daebfecc3
Ahmed <ahmed.jellouli@epfl.ch>
2021-05-14 14:56:54 +0200

    documentation app/patterns/search.py

--- app/patterns/search.py
+++ app/patterns/search.py
@@ -5,5 +5,11 @@ SUPPORTED_PATTERNS = [pattern.Deixis, pattern.DeixisJe, pattern.DeixisTu, patter
 
 
 def search_patterns(lemma: str, postag: str) -> list:
+    """
+    Main entrypoint to the patterns module. Returns a list of all patterns that match a given word
+    :param lemma: word in its lemmatized form, with accents
+    :param postag: part-of-speech tag of the word, following Perseids format (8 characters)
+    :return: List of all patterns that match the word
+    """
     movements = [p.name() for p in SUPPORTED_PATTERNS if p.matches(lemma, postag)]
     return movements
