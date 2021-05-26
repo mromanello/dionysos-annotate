a137d5dc5fffcab643913cb9ce72732ae8e476bd
Ahmed <ahmed.jellouli@epfl.ch>
2021-05-26 17:19:50 +0200

    add migrations folder to database

--- boot.sh
+++ boot.sh
@@ -3,11 +3,5 @@
 python -m venv venv
 venv/bin/pip install -q -r requirements.txt
 source venv/bin/activate
-if ! [[ -d migrations ]]
-then
-  flask db init > /dev/null 2>&1
-  flask db migrate > /dev/null 2>&1
-  flask db upgrade > /dev/null 2>&1
-fi
 cd app
 flask run
